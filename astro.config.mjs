3594356a6511f2393fa3413ceaddfd80f732b6e8
Marius S <39998+winding-lines@users.noreply.github.com>
2025-01-20 11:50:11 -0800

    Added tailwind, changed icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
   site: 'https://typed-data.dev',
-  integrations: [
-    preact(),
-    sitemap({
-      canonicalURL: 'https://typed-data.netlify.app'
-    })
-  ],
+  integrations: [preact(), sitemap({
+    canonicalURL: 'https://typed-data.netlify.app'
+  }), tailwind()],
 });
